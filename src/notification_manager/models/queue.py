a5c052c36f31b220bf2ea2bc4b6b3226ebf86cf2
diego.s <diego.s@hopu.eu>
2021-07-01 10:00:28 +0200

    Actualizados modelos del notification manager

--- src/notification_manager/models/queue.py
+++ src/notification_manager/models/queue.py
@@ -1,12 +1,16 @@
+from typing import List
+
+
 class Queue:
     """
     Class defining a subscription of a user (owner) with a query and or data category of the semantic engine.
     """
-    def __init__(self, _id: str, name: str):
+    def __init__(self, _id: str, name: str, endpoint: List[str] = None):
         self.id = _id
         self.name = name
+        self.endpoint = endpoint
         self.active = True
 
     def to_json(self):
-        json_out = {"id": self.id, "name": self.name, "active": self.active}
+        json_out = {"id": self.id, "name": self.name, "endpoint": self.endpoint, "active": self.active}
         return json_out
