7199c771860bdfa6d597cc3d6e22f1ab8970a510
Eleazar <eleazar@hopu.eu>
2021-07-02 14:10:55 +0200

    implemented initial functionality of notifications controller

--- src/notification_manager/controller/notifications_controller.py
+++ src/notification_manager/controller/notifications_controller.py
@@ -1,17 +1,23 @@
+import requests
+
 from src.notification_manager.models.notification import Notification
-from src.notification_manager.storage.services_queue_storage import ServicesQueueStorage
-from src.notification_manager.models.service import Service, service_to_object
+from src.notification_manager.models.queue_types import QueueType
 
 
 class NotificationsController:
-    def __init__(self, storage: ServicesQueueStorage):
-        self.storage = storage
 
-    def send_notification_service(self, destiny: list, data: dict = None):
+    def send_notification_service(self, queue_name, destiny: list, data: dict = None):
         # se crea una notification por cada destino con ¿origen?
-        for d in destiny:
-            notification = Notification(action="New Search Hits",
-                                        status="Ok",
-                                        origin="i3-market",
-                                        receptor=d,
-                                        data=data)
+
+        if queue_name == QueueType.NEWOFFERING.value:
+
+            for d in destiny:
+                endpoint = d.get('endpoint')
+                notification = Notification(action="New Search Hits",
+                                            status="Ok",
+                                            origin="i3-market",
+                                            receptor=d.get('name'),
+                                            data=data)
+                requests.post(url=endpoint, json=notification.to_json())
+        else:
+            return None
