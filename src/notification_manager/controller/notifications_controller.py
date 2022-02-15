c764b835431316a274307db59f0dc783a928bafb
eleazar_escudero <15699113Es_1>
2022-02-15 14:01:20 +0100

    Error control improved create service notification method which will return the requests response code

--- src/notification_manager/controller/notifications_controller.py
+++ src/notification_manager/controller/notifications_controller.py
@@ -3,7 +3,7 @@ import uuid
 
 import requests
 
-from src.notification_manager.models.notification import Notification
+from src.notification_manager.models.notification import Notification, notification_to_object
 from src.notification_manager.models.queue_types import QueueType
 from src.notification_manager.storage.notifications_storage import NotificationsStorage
 from loguru import logger
@@ -15,27 +15,50 @@ class NotificationsController:
         self.storage = storage
         self.web_ui = web_ui
 
-    @staticmethod
-    def send_notification_service(self, queue_name, destiny: dict, data: dict = None):
-        # se crea una notification por cada destino con ¿origen?
+    def create_service_notification(self, queue_name, queues, message):
+        # create the notification and send to them
+        return self.send_notification_service(queue_name, queues, message)
 
-        if queue_name == QueueType.NEWOFFERING.value:
-            for receptor_name, endpoint in destiny.items():
-                logger.info("Creating a notification to {} endpoint {}".format(receptor_name, endpoint))
-                notification = Notification(id=uuid.uuid4().__str__(),
-                                            action="New Search Hits",
-                                            status="Ok",
-                                            origin="i3-market",
-                                            receptor=receptor_name,
-                                            data=data)
+    @staticmethod
+    def send_notification_service(queue_name, destiny: dict, data: dict = None):
+        response = []
+        for receptor_name, endpoint in destiny.items():
+            logger.info("Creating a notification to {} endpoint {}".format(receptor_name, endpoint))
+            notification = None
+            if queue_name == QueueType.NEWOFFERING.value:
+                notification = Notification.new_offering_notification(receptor_name=receptor_name, data=data)
+
+            elif queue_name == QueueType.UPDATEOFFERING.value:
+                notification = Notification.update_offering_notification(receptor_name=receptor_name, data=data)
+
+            # Agreements are currently sent as user notifications
+            # elif queue_name == QueueType.AGREEMENTUPDATE.value:
+            #     pass
+            # elif queue_name == QueueType.AGREEMENTACCEPTED.value:
+            #     pass
+            # elif queue_name == QueueType.AGREEMENTPENDING.value:
+            #     pass
+            # elif queue_name == QueueType.AGREEMENTREJECTED.value:
+            #     pass
+            # elif queue_name == QueueType.AGREEMENTTERMINATION.value:
+            #     pass
+
+            # Only this is used, sent to conflict resolution.
+            elif queue_name == QueueType.AGREEMENTCLAIM.value:
+                pass
+
+            if notification is not None:
                 resp = requests.post(url=endpoint, json=notification.to_json())
-                logger.info("Notification service response: {}".format(resp))
-        else:
-            return None
+                response.append(
+                        {"destiny": receptor_name, "response": resp.status_code})
+                #logger.info("Notification service response: {}".format(resp))
+
+        return response
 
     def send_notification_user(self, destiny_user_id: str, origin: str, status: str, _type: str, predefined: bool,
                                message: dict = None):
-        notification = Notification(id=uuid.uuid4().__str__(),
+
+        notification = Notification(_id=uuid.uuid4().__str__(),
                                     action=_type,
                                     status=status,
                                     origin=origin,
@@ -44,16 +67,16 @@ class NotificationsController:
         return self.storage.insert_notification(notification.to_json())
 
     def get_user_notification(self, user_id):
-        return self.storage.retrieve_notification_by_user(user_id)
+        return [notification_to_object(notification).to_json() for notification in self.storage.retrieve_notification_by_user(user_id)]
 
     def get_unread_user_notification(self, user_id):
-        return self.storage.retrieve_unread_notification_by_user(user_id)
+        return [notification_to_object(notification).to_json() for notification in self.storage.retrieve_unread_notification_by_user(user_id)]
 
     def get_all_notifications(self):
-        return self.storage.retrieve_all()
+        return [notification_to_object(notification).to_json() for notification in self.storage.retrieve_all()]
 
     def get_all_unread_notifications(self):
-        return self.storage.retrieve_all_unread()
+        return [notification_to_object(notification).to_json() for notification in self.storage.retrieve_all_unread()]
 
     def get_notification(self, notification_id):
         # notifications = self.storage.retrieve_notification_by_user(user_id)
@@ -63,11 +86,17 @@ class NotificationsController:
         # return None
         notification = self.storage.retrieve_notification(notification_id)
         if notification:
-            return notification
+            return notification_to_object(notification).to_json()
         return None
 
     def modify_notification(self, notification_id, read):
-        return self.storage.read_notification(notification_id, read)
+        notif = self.storage.read_notification(notification_id, read)
+        if notif:
+            return notification_to_object(notif).to_json()
+        return notif
 
     def delete_notification(self, notification_id):
-        return self.storage.delete_notification(notification_id)
+        deleted_notif = self.storage.delete_notification(notification_id)
+        if deleted_notif:
+            return notification_to_object(deleted_notif).to_json()
+        return deleted_notif  # result is None
