80d2165a65580fe3782c48876cc28e97950c4565
eleazar_escudero <15699113Es_1>
2022-04-12 13:36:44 +0200

    added new tests

--- tests/functional/test_notifications.py
+++ tests/functional/test_notifications.py
@@ -1,5 +1,6 @@
 import uuid
 
+import market as market
 import notification as notification
 from _pytest.recwarn import warns
 
@@ -24,11 +25,18 @@ user_notifications = {
         }
 }
 
+market_id = 'market1'
+
 service_notifications = {
     '1':
         {
             "receiver_id": "offering.new",
             "message": {"msg": "this is a message!"}
+        },
+    '2':
+        {
+            "receiver_id": "agreement.claim",
+            "message": {"marketId": market_id, "msg": "this is a message!"}
         }
 }
 
@@ -64,10 +72,10 @@ def test_create_notification_with_empty_body_return_400_error_incomplete(client)
     assert response.status_code == BODY_ERROR
     assert response.json == {'detail': {
         'json': {f'{list(body.keys())[0]}': ['Unknown field.'], 'receiver_id': ['Missing data for required field.']}},
-                             'message': 'Validation error'}
+        'message': 'Validation error'}
 
 
-def test_create_notification_with_body_return_created(client):
+def test_create_user_notification_with_body_return_created(client):
     global notification_id
     response = client.post(f'{BASE_API}/notification', json=user_notifications.get('1'))
 
@@ -80,156 +88,83 @@ def test_create_notification_with_body_return_created(client):
 
 
 # RETRIEVE EXISTING USER NOTIFICATIONS
-# def test_retrieve_all_notifications_return_notification_lists(client):
-#    global notification_id
-#    response = client.get(f'{BASE_API}/notification/user/{user_notifications.get("1").get("receiver_id")}')
-#    response_body = response.json
-#    del response_body['id']
-#    assert response.status_code == OK
-#    #assert response_body == output_notification
+def test_retrieve_all_notifications_return_notification_lists(client):
+    global notification_id
+    response = client.get(f'{BASE_API}/notification/user/{user_notifications.get("1").get("receiver_id")}')
+    response_body = response.json
+    del response_body[0]['id']
+    assert response.status_code == OK
+    assert response_body == [output_notification]
 
 
-#  def test_retrieve_missing_notification_return_404_error_not_found(client):
-#     response = client.get(f'{BASE_API}/notification/user/random')
-#     assert response.status_code == 404
-#     assert response.json == NOT_FOUND_BODY
-#
-#
-# def test_retrieve_notification_by_id_return_object(client):
-#    global notification_id
-#    # get all registered services
-#    path = f'{BASE_API}/notification'
-#    response = client.get(path)
-#    assert response.status_code == OK
-#    # get a service id.
-#    notification_id = response.json[0].get('id')
-#    # get that service
-#    response = client.get(f'{BASE_API}/notification/{notification_id}')
-#    assert response.status_code == OK
-#    response_content = response.json
-#    del response_content['id']
-#    assert response_content == user_notifications.get('1')
-#
-#
-## CREATE QUEUES
-# def test_post_queue_to_non_exist_404_error(client):
-#    unexisting_notification = 'null'
-#    response = client.get(f'{BASE_API}/notification/{unexisting_notification}/queues')
-#    assert response.status_code == NOT_FOUND
-#    assert response.json == NOT_FOUND_BODY
-#
-#
-# def test_post_queue_exist_validation_error(client):
-#    bad_queue = {
-#        "name": "offering",
-#        "endpoint": None
-#    }
-#    response = client.post(f'{BASE_API}/notification/{notification_id}/queues', json=bad_queue)
-#    assert response.status_code == BODY_ERROR
-#    assert response.json == QUEUE_ERROR
-#
-#
-# def test_get_notification_queues_empty_list(client):
-#    response = client.get(f'{BASE_API}/notification/{notification_id}/queues')
-#    assert response.status_code == OK
-#    assert response.json == []
-#
-#
-# def test_post_queue(client):
-#    global queue_id
-#    response = client.post(f'{BASE_API}/notification/{notification_id}/queues', json=service_notifications)
-#    assert response.status_code == OK
-#    response_body = response.json
-#    queue_id = response_body['id']
-#    del response_body['id']
-#    assert response_body == service_notifications
-#
-#
-## GET QUEUE
-# def test_get_queue_not_exist(client):
-#    _uuid = uuid.uuid4().__str__()
-#    print(f'service id: {notification_id}')
-#    print(f'uuid: {_uuid}')
-#    path = f'{BASE_API}/notification/{notification_id}/queues/{_uuid}'
-#    print(path)
-#    response = client.get(path)
-#    assert response.status_code == NOT_FOUND
-#    assert response.json == NOT_FOUND_BODY
-#
-#
-# def test_get_queue_exist(client):
-#    response = client.get(f'{BASE_API}/notification/{notification_id}/queues/{queue_id}')
-#    assert response.status_code == OK
-#    response_body = response.json
-#    del response_body['id']
-#    assert response_body == service_notifications
-#
-#
-## GET ALL QUEUES
-# def test_get_notification_queues(client):
-#    response = client.get(f'{BASE_API}/notification/{notification_id}/queues')
-#    assert response.status_code == OK
-#    response_body = response.json
-#    del response_body[0]['id']
-#    assert response_body == [service_notifications]
-#
-#
-## MODIFY QUEUE
-# def test_deactivate_queue_dont_exist(client):
-#    response = client.patch(f'{BASE_API}/notification/{notification_id}/queues/{uuid.uuid4().__str__()}/deactivate')
-#    assert response.status_code == NOT_FOUND
-#    assert response.json == NOT_FOUND_BODY
-#
-#
-# def test_activate_queue_dont_exist(client):
-#    response = client.patch(f'{BASE_API}/notification/{notification_id}/queues/{uuid.uuid4().__str__()}/activate')
-#    assert response.status_code == NOT_FOUND
-#    assert response.json == NOT_FOUND_BODY
-#
-#
-# def test_deactivate_queue(client):
-#    response = client.patch(f'{BASE_API}/notification/{notification_id}/queues/{queue_id}/deactivate')
-#    assert response.status_code == OK
-#    response_body = response.json
-#    del response_body['id']
-#    inverted_bool = service_notifications.copy()
-#    inverted_bool['active'] = not inverted_bool['active']
-#    assert inverted_bool == response_body
-#
-#
-# def test_activate_queue(client):
-#    response = client.patch(f'{BASE_API}/notification/{notification_id}/queues/{queue_id}/activate')
-#    assert response.status_code == OK
-#    response_body = response.json
-#    del response_body['id']
-#    assert response_body == response_body
-#
-#
-## DELETE QUEUES
-# def test_delete_queue_dont_exist(client):
-#    response = client.delete(f'{BASE_API}/notification/{notification_id}/queues/{uuid.uuid4().__str__()}')
-#    assert response.status_code == NOT_FOUND
-#    assert response.json == NOT_FOUND_BODY
-#
-#
-# def test_delete_queue_exist(client):
-#    response = client.delete(f'{BASE_API}/notification/{notification_id}/queues/{queue_id}')
-#    assert response.status_code == OK
-#    response_body = response.json
-#    del response_body['id']
-#    assert response_body == service_notifications
-#
-#
-# def test_delete_notification_dont_exist(client):
-#    response = client.delete(f'{BASE_API}/notification/{uuid.uuid4().__str__()}')
-#    assert response.status_code == NOT_FOUND
-#    assert response.json == NOT_FOUND_BODY
-#
-#
-# def test_delete_notification_exist(client):
-#    response = client.delete(f'{BASE_API}/notification/{notification_id}')
-#    assert response.status_code == OK
-#    response_body = response.json
-#    del response_body['id']
-#    assert response_body == user_notifications.get('1')
-#
+def test_retrieve_missing_notification_return_404_error_not_found(client):
+    response = client.get(f'{BASE_API}/notification/user/random')
+    assert response.status_code == 404
+    assert response.json == NOT_FOUND_BODY
+
+
+def test_retrieve_notification_by_id_return_object(client):
+    global notification_id
+    # get all registered services
+    path = f'{BASE_API}/notification'
+    response = client.get(path)
+    assert response.status_code == OK
+    # get a service id.
+    notification_id = response.json[0].get('id')
+    # get that service
+    response = client.get(f'{BASE_API}/notification/{notification_id}')
+    assert response.status_code == OK
+    response_content = response.json
+    del response_content['id']
+    assert response_content == output_notification
+
+
+# TODO MODIFY USER NOTIFICATIONS
+# TODO ADD SERVICE NOTIFICATIONS
+# MARKETPLACE NOTIFICATIONS
+services = {
+    '1':
+        {
+            "endpoint": "http://localhost:2000",
+            "name": "service-test",
+            "marketId": market_id,
+            'queues': []
+        }
+}
+
+queue = {
+    "name": "agreement.claim",
+    "endpoint": None,
+    "active": True
+}
+
+service_id = ''
+queue_id = ''
+
+
+def test_marketplace_service_notification_200_success(client):
+    # register service
+    # to terminal ➜ nc -l -p 2000 -k
+    global service_id
+    response = client.post(f'{BASE_API}/services', json=services.get('1'))
+    response_body = response.json
+    service_id = response_body.get('id')
+    if service_id:
+        del response_body['id']
+
+    assert response.status_code == OK
+    assert response_body == services.get('1')
+
+    # register queue
+    global queue_id
+    response = client.post(f'{BASE_API}/services/{service_id}/queues', json=queue)
+    assert response.status_code == OK
+    response_body = response.json
+    queue_id = response_body['id']
+    if queue_id:
+        del response_body['id']
+    assert response_body == queue
+    # create service notitication
+    response = client.post(f'{BASE_API}/notification/service', json=service_notifications['2'])
+    response_body = response.json
+    assert response_body == [{'destiny': services.get('1').get('name'), 'response': 'error'}]
