2175c2a8333fa973d19b8b8ed623cb41de2aaa44
eleazar_escudero <15699113Es_1>
2022-10-11 13:11:07 +0200

    improved the way of getting queue_types

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,6 @@
+from src.notification_manager.models import queue_types
+from src.notification_manager.models.queue_types import QueueType
+
 OK_CODE = 200
 NOT_FOUND_CODE = 404
 BODY_ERROR_CODE = 400
@@ -6,8 +9,12 @@ ALREADY_EXIST_BODY = {'detail': {}, 'message': 'Already exists subscription to c
 ALREADY_EXIST_SERVICE_BODY = {'detail': {}, 'message': 'Already exists'}
 NOT_FOUND_BODY = {'detail': {}, 'message': 'Not Found'}
 # INCOMPLETE_BODY = {'detail': {}, 'message': 'Incomplete Body'}
-QUEUE_ERROR_BODY = {'detail': {'json': {'name': ['Must be one of: offering.new, offering.update, '
-                                            'agreement.accepted, agreement.rejected, '
-                                            'agreement.update, agreement.pending, '
-                                            'agreement.termination, agreement.claim.']}},
-               'message': 'Validation error'}
+queues_type = ', '.join([q.value for q in QueueType.__members__.values()]) + '.'
+QUEUE_ERROR_BODY = {'detail': {'json': {'name': [f"Must be one of: {queues_type}"
+                                                 # 'offering.new, offering.update, '
+                                                 # 'agreement.accepted, agreement.rejected, '
+                                                 # 'agreement.update, agreement.pending, '
+                                                 # 'agreement.termination, agreement.claim.'
+
+                                                 ]}},
+                    'message': 'Validation error'}
